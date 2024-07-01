109ce41021f22bc5ac3dc76760e9de9ffd2cd33f
Amru Muiz Fauzan <163871761+LordZerav@users.noreply.github.com>
2024-07-01 15:34:34 +0700

    Update (80%)
    Belum kelar

--- Sistem Peminjaman Perpustakaan.cpp
+++ Sistem Peminjaman Perpustakaan.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <conio.h>
-#include <stdlib.h>
 #include <vector>
 
 using namespace std;
 
-
 class Buku {
-    public:
-        string judul;
-        string pengarang;
-        string idbuku;
-        int no;
-        int tahunterbit;
-        int stokbuku;
-        bool statusbuku;
-};
+public:
+    string judul;
+    string pengarang;
+    string idbuku;
+    int no;
+    int tahunterbit;
+    int stokbuku;
+    bool statusbuku;
 
-class Peminjam {
-    public:
-        string namapeminjam;
-        string asaldaerahpeminjam;
-        string nomorteleponpeminjam;
-        int idpeminjam;
+    Buku() : no(0), tahunterbit(0), stokbuku(0), statusbuku(false) {}
+
+    string toString() const {
+        return judul + ";" + pengarang + ";" + idbuku + ";" + to_string(no) + ";" + to_string(tahunterbit) + ";" + to_string(stokbuku) + ";" + (statusbuku ? "1" : "0") + "\n";
+    }
 };
 
 class Perpustakaan {
 public:
     vector<Buku> bukuList;
-    const string fileName = "file.txt";
+    const string fileName = "DBase.txt";
 
     void bacaData() {
         ifstream file(fileName);
@@ -38,14 +33,17 @@ public:
 
         string line;
         while (getline(file, line)) {
-            string judul, pengarang, tahunStr, dipinjamStr;
+            Buku buku;
             size_t pos = 0;
-            pos = line.find(";"); judul = line.substr(0, pos); line.erase(0, pos + 1);
-            pos = line.find(";"); pengarang = line.substr(0, pos); line.erase(0, pos + 1);
-            pos = line.find(";"); tahunStr = line.substr(0, pos); line.erase(0, pos + 1);
-            dipinjamStr = line;
+            pos = line.find(";"); buku.judul = line.substr(0, pos); line.erase(0, pos + 1);
+            pos = line.find(";"); buku.pengarang = line.substr(0, pos); line.erase(0, pos + 1);
+            pos = line.find(";"); buku.idbuku = line.substr(0, pos); line.erase(0, pos + 1);
+            pos = line.find(";"); buku.no = stoi(line.substr(0, pos)); line.erase(0, pos + 1);
+            pos = line.find(";"); buku.tahunterbit = stoi(line.substr(0, pos)); line.erase(0, pos + 1);
+            pos = line.find(";"); buku.stokbuku = stoi(line.substr(0, pos)); line.erase(0, pos + 1);
+            buku.statusbuku = (line == "1");
 
-            bukuList.emplace_back(judul, pengarang, std::stoi(tahunStr), dipinjamStr == "1");
+            bukuList.push_back(buku);
         }
         file.close();
     }
@@ -58,11 +56,17 @@ public:
         file.close();
     }
 
-    void tambahBuku(vector<Buku>& dataBuku, int banyakData) {
-        for (int a = 0; a < banyakData; ++a) {
+    void tambahBuku() {
+        int banyakBuku;
+        cout << "\n";
+        cout << "Masukkan jumlah buku yang ingin ditambahkan: ";
+        cin >> banyakBuku;
+
+        for (int a = 0; a < banyakBuku; ++a) {
+            cout << "\n";
             cout << "Data Buku Ke-" << a + 1 << ":\n";
             Buku buku;
-            buku.no = a + 1;
+            buku.no = bukuList.size() + 1;
 
             cout << "Masukkan ID Buku\t: ";
             cin.ignore();
@@ -80,97 +84,131 @@ public:
             cout << "Masukkan Stok Buku\t: ";
             cin >> buku.stokbuku;
 
-            dataBuku.push_back(buku);
+
+            buku.statusbuku = false;
+            bukuList.push_back(buku);
+        }
+        cout <<"\n";
+        cout <<"Data Buku telah disimpan\n";
+        cout <<"\n";
+        simpanData();
+    }
+
+    void cariBuku() {
+        bacaData();
+        Buku buku;
+        string judul;
+        cout << "\n";
+        cout << "Masukkan Judul Buku yang Dicari: ";
+        cin >> judul;
+        cout << "\n";
+        getline(cin, judul);
+
+        for (auto &buku : bukuList) {
+            if (buku.judul == judul) {
+                    cout << "Judul Buku\t : " << buku.judul << "\n";
+                    cout << "Pengarang\t : " << buku.pengarang << "\n";
+                    cout << "Diterbitkan\t : " << buku.tahunterbit << "\n";
+                    cout << "Status Buku\t : " << (buku.statusbuku ? "Dipinjam" : "Tersedia") << "\n";
+                    cout << "\n";
+                return ;
+            }
+        }
+
+        cout << "Buku Tidak Ditemukan\n";
+        cout << "\n";
+    }
+
+    void editBuku() {
+        string judul;
+        cout << "\n";
+        cout << "Masukkan Judul Buku yang Ingin Diedit: ";
+        cin >> judul;
+        getline(cin, judul);
+
+        for (auto &buku : bukuList) {
+            if (buku.judul == judul) {
+                cout << "Masukkan Pengarang Baru: ";
+                getline(cin, buku.pengarang);
+                cout << "Masukkan Tahun Terbit Baru: ";
+                cin >> buku.tahunterbit;
+                simpanData();
+                return;
+            }
+        }
+        cout << "Buku Tidak Ditemukan\n";
+    }
+
+    void pinjamBuku() {
+        string judul;
+        cout << "\n";
+        cout << "Masukkan Judul Buku yang Ingin Dipinjam: ";
+        cin >> ws;
+        getline(cin, judul);
+
+        for (auto &buku : bukuList) {
+            if (buku.judul == judul && !buku.statusbuku) {
+                buku.statusbuku = true;
+                simpanData();
+                cout << "Buku Berhasil Dipinjam\n";
+                return;
+            }
+        }
+        cout << "Buku Tidak Ditemukan atau Sudah Dipinjam\n";
+    }
+
+    void kembalikanBuku() {
+        string judul;
+        cout << "\n";
+        cout << "Masukkan Judul Buku yang Ingin Dikembalikan: ";
+        cin >> ws;
+        getline(cin, judul);
+
+        for (auto &buku : bukuList) {
+            if (buku.judul == judul && buku.statusbuku) {
+                buku.statusbuku = false;
+                simpanData();
+                cout << "Buku Berhasil Dikembalikan\n";
+                return;
+            }
+        }
+        cout << "Buku Tidak Ditemukan atau Belum Dipinjam\n";
+    }
+
+    void tampilkanBuku() {
+        cout << "\n";
+        cout << "Daftar Buku di Perpustakaan:\n";
+        cout << "\n";
+        for (auto &buku : bukuList) {
+            cout << "Judul Buku\t : " << buku.judul << "\n";
+            cout << "Pengarang\t : " << buku.pengarang << "\n";
+            cout << "Diterbitkan\t : " << buku.tahunterbit << "\n";
+            cout << "Status Buku\t : " << (buku.statusbuku ? "Dipinjam" : "Tersedia") << "\n";
+            cout << "\n";
         }
     }
 
-
-    void cariBuku() {
-            string judul;
-            cout << "Masukkan Judul Buku yang Dicari: ";
-            cin >> ws; getline(cin, judul);
-
-            for (auto &buku : bukuList) {
-                if (buku.judul == judul) {
-                    cout << "Buku Ditemukan: " << buku.judul << ", " << buku.pengarang << ", " << buku.tahun << ", " << (buku.statusbuku ? "Dipinjam" : "Tersedia") << "\n";
-                    return;
-                }
-            }
-            cout << "Buku Tidak Ditemukan\n";
-        }
-
-    void editBuku() {
-            string judul;
-            cout << "Masukkan Judul Buku yang Ingin Diedit: ";
-            cin >> ws; getline(cin, judul);
-
-            for (auto &buku : bukuList) {
-                if (buku.judul == judul) {
-                    cout << "Masukkan Pengarang Baru: ";
-                    getline(cin, buku.pengarang);
-                    cout << "Masukkan Tahun Terbit Baru: ";
-                    cin >> buku.tahun;
-                    simpanData();
-                    return;
-                }
-            }
-            cout << "Buku Tidak Ditemukan\n";
-        }
-
-    void pinjamBuku() {
-            string judul;
-            cout << "Masukkan Judul Buku yang Ingin Dipinjam: ";
-            cin >> ws; getline(cin, judul);
-
-            for (auto &buku : bukuList) {
-                if (buku.judul == judul && !buku.statusbuku) {
-                    buku.statusbuku = true;
-                    simpanData();
-                    cout << "Buku Berhasil Dipinjam\n";
-                    return;
-                }
-            }
-            cout << "Buku Tidak Ditemukan atau Sudah Dipinjam\n";
-        }
-
-    void kembalikanBuku() {
-            string judul;
-            cout << "Masukkan Judul Buku yang Ingin Dikembalikan: ";
-            cin >> ws; getline(cin, judul);
-
-            for (auto &buku : bukuList) {
-                if (buku.judul == judul && buku.statusbuku) {
-                    buku.statusbuku = false;
-                    simpanData();
-                    cout << "Buku Berhasil Dikembalikan\n";
-                    return;
-                }
-            }
-            cout << "Buku Tidak Ditemukan atau Belum Dipinjam\n";
-        }
-
-    void tampilkanBuku() {
-            cout << "Daftar Buku di Perpustakaan:\n";
-            for (auto &buku : bukuList) {
-                cout << buku.judul << ", " << buku.pengarang << ", " << buku.tahun << ", " << (buku.dipinjam ? "Dipinjam" : "Tersedia") << "\n";
-            }
-        }
-
     void keluar() {
-            cout << "Terima kasih telah menggunakan layanan perpustakaan.\n";
-        }
+        cout << "\n";
+        cout << "Terima kasih telah menggunakan layanan perpustakaan.\n";
+    }
 };
 
-int n;
-int banyakdata;
-
-int main () {
-    while(true) {
-
-        cout << ":::::::::::::::: SELAMAT DATANG DI PERPUSTAKAAN PUTRA BANGSA ::::::::::::::::\n";
-        cout << "-----------------------------------------------------------------------------\n";
+int main() {
+    Perpustakaan perpustakaan;
+    perpustakaan.bacaData();
+    
+    while (true) {
+        cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
+        cout << "\n";
+        cout << " \t \t SELAMAT DATANG DI PERPUSTAKAAN PUTRA BANGSA \t \t \n";
+        cout << "\n";
+        cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
+        cout << "\n";
 
         cout << "Silahkan Anda bisa memilih layanan berikut ini :\n";
+        cout << "\n";
+        
         cout << "[1] Menambahkan Buku Baru \n";
         cout << "[2] Mencari Buku\n";
         cout << "[3] Mengedit Informasi Buku\n";
@@ -178,34 +216,37 @@ int main () {
         cout << "[5] Mengembalikan Buku\n";
         cout << "[6] Menampilkan Daftar Buku\n";
         cout << "[7] Keluar\n";
-        cout << "\n";
+        cout << "\nSilahkan tentukan pilihan Anda [1] - [7]\n";
         
-        cout << "Silahkan tentukan pilihan Anda [1] - [7]\n";
+        int n;
         cin >> n;
 
-        if (n == 1) {
-                Perpustakaan.tambahBuku;
-            } else if (n == 2) {
-                Perpustakaan.cariBuku;
-
-            } else if (n == 3) {
-                Perpustakaan.editBuku;
-
-            } else if (n == 4) {
-                Perpustakaan.pinjamBuku;
-
-            } else if (n == 5) {
-                Perpustakaan.kembalikanBuku;
-
-            } else if (n == 6) {
-                Perpustakaan.tampilkanBuku;
+        switch (n) {
+            case 1:
+                perpustakaan.tambahBuku();
                 break;
-
-            } 
-            else {
-                cout << "Pilihan Anda tidak valid, Silakan coba lagi/\n";
-            } 
+            case 2:
+                perpustakaan.cariBuku();
+                break;
+            case 3:
+                perpustakaan.editBuku();
+                break;
+            case 4:
+                perpustakaan.pinjamBuku();
+                break;
+            case 5:
+                perpustakaan.kembalikanBuku();
+                break;
+            case 6:
+                perpustakaan.tampilkanBuku();
+                break;
+            case 7:
+                perpustakaan.keluar();
+                return 0;
+            default:
+                cout << "Pilihan Anda tidak valid, Silakan coba lagi.\n";
+        }
     }
 
-    return 0;
+return 0;
 }
